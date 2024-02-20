302cadb31d03ea401cfd6b6fba04117ad3413754
Alex Triteckiy <alextrytetskyi@Alexs-MacBook-Pro.local>
2024-02-20 17:17:21 +0200

    add sm fixes

--- frontend/src/app/features/classic-techniques/classic-techniques.module.ts
+++ frontend/src/app/features/classic-techniques/classic-techniques.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ClassicTechniquesRoutingModule } from './classic-techniques-routing.module';
 import { ClassicTechniquesComponent } from './page/classic-techniques.component';
+import { COMPONENTS } from './modules';
 
 @NgModule({
-  declarations: [ClassicTechniquesComponent],
+  declarations: [ClassicTechniquesComponent, ...COMPONENTS],
   imports: [CommonModule, ClassicTechniquesRoutingModule],
 })
 export class ClassicTechniquesModule {}
